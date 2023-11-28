1086c32fcaf721e7d55b48663e475cb9ca3f851f
inane-phe <127912578+inane-phe@users.noreply.github.com>
2023-11-28 17:04:09 +0800

    Add files via upload

---
+++ Snowflake/Snowflake/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Snowflake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.counter1 = new System.Windows.Forms.NumericUpDown();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.counter1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // counter1
+            // 
+            this.counter1.Location = new System.Drawing.Point(12, 35);
+            this.counter1.Name = "counter1";
+            this.counter1.Size = new System.Drawing.Size(162, 22);
+            this.counter1.TabIndex = 0;
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(6, 17);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(935, 489);
+            this.picBox.TabIndex = 1;
+            this.picBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picBox);
+            this.groupBox1.Location = new System.Drawing.Point(0, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(942, 506);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "DRAW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.counter1);
+            this.groupBox2.Location = new System.Drawing.Point(0, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(942, 61);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "線條粗細";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(180, 30);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 12;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(771, 32);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "結束";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(771, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 21);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "自動旋轉";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(718, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(852, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "重置";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "分形次數";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "旋轉速度";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(661, 32);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(852, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "旋轉";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 551);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.counter1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown counter1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Snowflake/Snowflake/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snowflake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Graphics g;
+        static Pen pen3, pen1, pen2;
+        int ro = 0;
+        PointF point1 = new PointF(240, 120);
+        PointF point2 = new PointF(720, 120);
+        PointF point3 = new PointF(480, 440);
+        
+        private float rotationAngle = 0.2f; // 20% 的旋轉角度
+        /// <summary>
+        /// Построение фрактала
+        /// </summary>
+        /// <param name="p1">Первая точка</param>
+        /// <param name="p2">Вторая точка</param>
+        /// <param name="p3">3 точка для координации</param>
+        /// <param name="lvl">Колличество итераций</param>
+        /// <returns></returns>
+        static int Fractal(PointF p1, PointF p2, PointF p3, int lvl)
+        {
+
+            if (lvl > 0)
+            {
+                //средняя треть отрезка
+                var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
+                var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+                //координаты вершины угла
+                var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);          //邊的中點
+                var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);  //突出的角
+                //рисуем его
+                g.DrawLine(pen1, p4, pn);
+                g.DrawLine(pen2, p5, pn);
+                g.DrawLine(pen3, p4, p5);
+
+
+                //рекурсивно вызываем функцию нужное число раз
+                Fractal(p4, pn, p5, lvl - 1);
+                Fractal(pn, p5, p4, lvl - 1);
+                Fractal(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), lvl - 1);
+                Fractal(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), lvl - 1);
+
+            }
+            return lvl;
+        }
+
+        private void Draw(int lvl)
+        {
+            SetOption();
+            DrawRectangle(ref g);
+            AllFractal(lvl);
+        }
+
+        private void AllFractal(int lvl)
+        {
+            Fractal(point1, point2, point3, lvl);
+            Fractal(point2, point3, point1, lvl);
+            Fractal(point3, point1, point2, lvl);
+        }
+
+        private void SetOption()
+        {
+            SetPens();
+            ClearPicBox();
+        }
+
+        private void SetPens()
+        {
+            pen1 = new Pen(Color.Blue, trackBar2.Value);
+            pen2 = new Pen(Color.Orange, trackBar2.Value);
+            pen3 = new Pen(Color.White, trackBar2.Value+1);
+            
+        }
+
+        private void ClearPicBox()
+        {
+            g = picBox.CreateGraphics();
+            g.Clear(Color.White);
+        }
+        private void DrawRectangle(ref Graphics g)
+        {
+            g.DrawLine(pen1, point1, point2);
+            g.DrawLine(pen1, point2, point3);
+            g.DrawLine(pen1, point3, point1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Draw(Convert.ToInt32(counter1.Value));
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ro = ro + 20;
+            Rotate(ro);
+        }
+        private void Rotate(double angle)
+        {
+            // 旋转角度
+            double angleInRadians = angle * Math.PI / 180.0;
+
+            // 获取当前图形的中心点
+            PointF center = new PointF((point1.X + point2.X + point3.X) / 3, (point1.Y + point2.Y + point3.Y) / 3);
+
+            // 旋转每个顶点
+            point1 = RotatePoint(point1, center, angleInRadians);
+            point2 = RotatePoint(point2, center, angleInRadians);
+            point3 = RotatePoint(point3, center, angleInRadians);
+
+            // 清空图形并重新绘制
+            ClearPicBox();
+            DrawRectangle(ref g);
+            AllFractal(Convert.ToInt32(counter1.Value));
+            picBox.Update();
+        }
+
+        private PointF RotatePoint(PointF point, PointF center, double angle)
+        {
+            float x = center.X + (float)((point.X - center.X) * Math.Cos(angle) - (point.Y - center.Y) * Math.Sin(angle));
+            float y = center.Y + (float)((point.X - center.X) * Math.Sin(angle) + (point.Y - center.Y) * Math.Cos(angle));
+
+            return new PointF(x, y);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            ResetRotation();
+        }
+        private void ResetRotation()
+        {
+            point1 = new PointF(240, 120);
+            point2 = new PointF(720, 120);
+            point3 = new PointF(480, 440);
+
+            // 清空图形并重新绘制
+            ClearPicBox();
+            DrawRectangle(ref g);
+            AllFractal(Convert.ToInt32(counter1.Value));
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            label3.Text = trackBar1.Value.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 100/trackBar1.Value;
+            ro = ro + 20;
+            Rotate(ro);
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = trackBar2.Value.ToString();
+        }       
+    }
+}
